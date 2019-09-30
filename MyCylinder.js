c9915dcffd7bee537e6def4bba0a95859069d2b8
Claudia <claudiainescostamartins@gmail.com>
2019-09-30 10:43:37 +0100

    Finished cylinder

---
+++ MyCylinder.js
@@ -0,0 +1,59 @@
+/**
+ * MyCylinder
+ * @constructor
+ */
+class MyCylinder extends CGFobject {
+    constructor(scene, id, height, base, top, slices, stacks) {
+        super(scene);
+
+        this.height = height;
+        this.base = base;
+        this.top = top;
+        this.slices = slices;
+        this.stacks = stacks;
+
+        this.initBuffers();
+    };
+
+    initBuffers() {
+        this.vertices = [];
+        this.indices = [];
+        this.normals = [];
+        this.texCoords = [];
+
+        var alphaAng = 2 * Math.PI / this.slices;
+        var heightStacks = this.height / this.stacks;
+        var sizeSlices = (this.top - this.base) / this.stacks;
+        
+        for (var i = 0; i <= this.slices; ++i) {
+            for (var j = 0; j <= this.stacks; ++j) {
+
+                this.vertices.push(
+                    (this.base + sizeSlices * j) * Math.cos(alphaAng * i),
+                    (this.base + sizeSlices * j) * Math.sin(alphaAng * i),
+                    j * heightStacks
+                );
+
+                if (j < this.stacks && i < this.slices) {
+                    this.indices.push(
+                        (i + 1) * (this.stacks + 1) + j, i * (this.stacks + 1) + j + 1, i * (this.stacks + 1) + j,
+                        i * (this.stacks + 1) + j + 1, (i + 1) * (this.stacks + 1) + j, (i + 1) * (this.stacks + 1) + j + 1
+                    );
+                }
+
+                this.texCoords.push(
+                    i * 1 / this.slices,
+                    1 - (j * 1 / this.stacks)
+                );
+
+                this.normals.push(
+                    Math.cos(alphaAng * i),
+                    Math.sin(alphaAng * i), 0
+                );
+            }
+        }
+
+        this.primitiveType = this.scene.gl.TRIANGLES;
+        this.initGLBuffers();
+    };
+};

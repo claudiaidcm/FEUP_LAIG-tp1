db6830ad084f43f557e084cb18923e5fc15cb51c
Claudia <claudiainescostamartins@gmail.com>
2019-09-30 14:09:26 +0100

    torus complete

--- MyTorus.js
+++ MyTorus.js
@@ -15,14 +15,46 @@ class MyTorus extends CGFobject {
         this.initBuffers();
     };
 
-    initBuffers() {
-        this.vertices = [];
-        this.indices = [];
-        this.normals = [];
-        this.texCoords = [];
-    
+	initBuffers() {
+		this.vertices = [];
+		this.indices = [];
+		this.normals = [];
+		this.texCoords = [];
 
-        this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
+		var alphaAng = 2*Math.PI/this.slices;
+		var loopAng = 2*Math.PI/this.loops;
+
+		for(var i = 0; i <= this.slices; ++i) {
+			for(var j = 0; j <= this.loops; ++j) {
+
+				this.vertices.push(
+					(this.outer + this.inner*Math.cos(loopAng*j)) * Math.cos(alphaAng*i), 
+					(this.outer + this.inner*Math.cos(loopAng*j)) * Math.sin(alphaAng*i), 
+					this.inner * Math.sin(loopAng*j)
+                );
+                
+                if (j < this.loops && i < this.slices) {
+                    this.indices.push(
+                        (i+1)*(this.loops+1) + j, i*(this.loops+1) + j+1, i*(this.loops+1) + j,
+                        i*(this.loops+1) + j+1, (i+1)*(this.loops+1) + j, (i+1)*(this.loops+1) + j+1
+                    );
+                }
+
+				this.texCoords.push(
+					i*1/this.slices, 
+					j*1/this.loops	
+				);
+
+				this.normals.push(
+					Math.cos(loopAng*j) * Math.cos(alphaAng*i), 
+                    Math.cos(loopAng*j) * Math.sin(alphaAng*i),
+                    0
+				);
+
+			}
+        }
+        
+		this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
     };
 };
